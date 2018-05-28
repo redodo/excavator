905ef820e740320d8cc68ca0fb04d75335dd0252
redodo <me@redodo.io>
2018-05-28 16:41:44 +0200

    Stuff

---
+++ dirtcastle/strategies/base.py
@@ -0,0 +1,22 @@
+class Strategy:
+
+    def apply(self, text):
+        raise NotImplementedError(
+            '%s does not implement the `apply` method'
+            % self.__class__.__name
+        )
+
+
+class CompleteStrategy(Strategy):
+
+    def apply(self, text):
+        for y, line in enumerate(text.lines):
+            for x, cell in enumerate(line.cells):
+                for annotation in cell:
+                    self.apply_to_annotation(text, line, annotation, x, y)
+
+    def apply_to_annotation(self, text, line, annotation, x, y):
+        raise NotImplementedError(
+            '%s does not implement the `apply_to_annotation` method'
+            % self.__class__.__name__
+        )
