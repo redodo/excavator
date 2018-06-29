7ddb7fe267729a71d6fdfe14866f77432b942194
redodo <me@redodo.io>
2018-06-29 11:57:52 +0200

    Stuff

--- dirtcastle/newapi/app.py
+++ dirtcastle/newapi/app.py
@@ -1,26 +1,64 @@
 import falcon
+import pymongo
 
 from .database import db
 
+
+class MongoMediaComponent:
+
+    PYMONGO_RESULT_CLASSES = (
+        pymongo.results.DeleteResult,
+        pymongo.results.InsertOneResult,
+        pymongo.results.UpdateResult,
+    )
+
+    def process_response(self, req, resp, resource, req_succeeded):
+        if not hasattr(resp, 'mongo'):
+            return
+
+        if resp.mongo is None:
+            raise falcon.HTTPNotFound(
+                description='helloworld'
+            )
+        elif isinstance(resp.mongo, pymongo.cursor.Cursor):
+            results = list(resp.mongo)
+            for result in results:
+                result.pop('_id')
+            resp.media = results
+        elif isinstance(resp.mongo, dict):
+            resp.mongo.pop('_id')
+            resp.media = resp.mongo
+        elif isinstance(resp.mongo, self.PYMONGO_RESULT_CLASSES):
+            pass
+        else:
+            raise NotImplementedError(
+                'resp.mongo is of unknown type %s'
+                % type(resp.mongo)
+            )
+ 
 
 class TokenCollection:
 
     def on_get(self, req, resp):
         """Lists all tokens"""
-        resp.media = {
-            token['name']: token['value']
-            for token in db.tokens.find()
-        }
-
-    def on_put(self, req, resp, name):
-        pass
+        resp.mongo = db.tokens.find()
 
 
 class TokenItem:
-    pass
+
+    def on_get(self, req, resp, name):
+        resp.mongo = db.tokens.delete_one({'name': name})
+        # resp.mongo = db.tokens.find_one({'name': name})
+
+    def on_put(self, req, resp, name):
+        value = req.media.get('pattern')
+        resp.mongo = db.tokens.insert_one({'name': name, 'pattern': value})
+
+    def on_delete(self, req, resp, name):
+        resp.mongo = db.tokens.delete_one({'name': name})
 
 
-app = falcon.API()
+app = falcon.API(middleware=[MongoMediaComponent()])
 app.add_route('/tokens', TokenCollection())
 app.add_route('/tokens/{name}', TokenItem())
 
